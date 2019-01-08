ae65a55bb7558dacd14970053bda86c3e8040aad
chubzielex <reyes.lexus@gmail.com>
2019-01-08 14:42:46 +0800

    resize

--- 001-basic_elements/BasicElements.java
+++ 001-basic_elements/BasicElements.java
@@ -6,7 +6,7 @@ public class BasicElements{
 	public static void main(String args[]){
 		JFrame frame = new JFrame("Login");
 
-        frame.setSize(300, 150);
+        frame.setSize(500, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JPanel panel = new JPanel();
@@ -23,25 +23,25 @@ public class BasicElements{
         panel.setBackground(Color.black);
 
         JLabel userLabel = new JLabel("Username:");
-        userLabel.setBounds(10, 10, 80, 25);
+        userLabel.setBounds(100, 60, 80, 25);
         userLabel.setForeground(Color.white);
         panel.add(userLabel);
 
         JTextField userText = new JTextField(20);
-        userText.setBounds(100, 10, 160, 25);
+        userText.setBounds(200, 60, 160, 25);
         panel.add(userText);
 
         JLabel passwordLabel = new JLabel("Password:");
-        passwordLabel.setBounds(10, 40, 80, 25);
+        passwordLabel.setBounds(100, 110, 80, 25);
         passwordLabel.setForeground(Color.white);
         panel.add(passwordLabel);
 
         JPasswordField passwordText = new JPasswordField(20);
-        passwordText.setBounds(100, 40, 160, 25);
+        passwordText.setBounds(200, 110, 160, 25);
         panel.add(passwordText);
 
-        JButton loginButton = new JButton("login");
-        loginButton.setBounds(10, 80, 80, 25);
+        JButton loginButton = new JButton("Login");
+        loginButton.setBounds(220, 180, 80, 25);
         panel.add(loginButton);
         
     }
