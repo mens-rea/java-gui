02b9aa029fe8d438298e7b887ff7972424e99f00
AmirAverilla <averilla1820133@mkt.ceu.edu.ph>
2019-01-08 15:11:02 +0800

    create log in form

--- 001-basic_elements/BasicElements.java
+++ 001-basic_elements/BasicElements.java
@@ -1,15 +1,34 @@
-import javax.swing.JFrame;
+import javax.swing.*;
 
 public class BasicElements{
-	
-	public static void main(String args[]){
-		JFrame myFrame = new JFrame();
 
+	public static void main (String[] args){
+		JFrame myFrame = new JFrame("Basic java Gui");
+		JButton myButton = new JButton("Login");
+		JTextField myTextField = new JTextField("Username");
+		JTextField myTextField2 = new JTextField("Password");
+
+
+
+		myTextField.setBounds(150, 50, 150, 30);
+		myTextField.setVisible(true);
+		myTextField.setLayout(null);
+
+		myTextField2.setBounds(150, 100, 150, 30);
+		myTextField2.setVisible(true);
+		myTextField2.setLayout(null);
+		
+		myButton.setBounds(150, 150, 150, 50);
+
+		myFrame.setLayout(null);
+		myFrame.add(myButton);
+		myFrame.add(myTextField);
+		myFrame.add(myTextField2);
 		myFrame.setDefaultCloseOperation(myFrame.EXIT_ON_CLOSE);
 		myFrame.setVisible(true);
-		myFrame.setSize(900, 600);
+		myFrame.setSize(500, 300);
 
 		System.out.println("basic java elements");
-	}
 
+	}
 }
