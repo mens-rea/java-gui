d605346882dec5521c95b81b1b4e936b6db54e1d
shanelly <shanelle.encomienda@gmail.com>
2019-01-08 15:33:44 +0800

    created log in form

--- 001-basic_elements/BasicElements.java
+++ 001-basic_elements/BasicElements.java
@@ -1,15 +1,45 @@
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.FlowLayout;
 
 public class BasicElements{
-	
-	public static void main(String args[]){
-		JFrame myFrame = new JFrame();
 
+	static JTextField username, password;
+
+	public static void main(String args[]){
+		JFrame myFrame = new JFrame("Log In");
+		myFrame.getContentPane().setLayout(new FlowLayout());
+
+		
+	    JLabel userlabel = new JLabel("Please enter your username and password");
+	    userlabel.setBounds(26, 41, 89, 22);
+	    myFrame.getContentPane().add(userlabel);
+
+		username = new JTextField("username",30);
+		username.setBounds(200, 110, 150, 500);
+
+
+		password = new JTextField("password",30);
+		password.setBounds(200, 110, 150, 500);
+
+
+		myFrame.getContentPane().add(username);
+		myFrame.getContentPane().add(password);
+
+		
+		JButton logIn= new JButton("Login");
+		logIn.setSize(200,50);
+		logIn.setBounds(267, 124, 136, 43);
+
+		JButton exit= new JButton("Exit");
+		exit.setSize(210,50);
+		exit.setBounds(267, 178, 136, 43);
+
+		myFrame.getContentPane().add(logIn);
+		myFrame.getContentPane().add(exit);
 		myFrame.setDefaultCloseOperation(myFrame.EXIT_ON_CLOSE);
 		myFrame.setVisible(true);
-		myFrame.setSize(900, 600);
+		myFrame.setSize(400, 200);
 
-		System.out.println("basic java elements");
 	}
 
 }
