d52b8979315642fe0a8eedff4ef560b89eb1e51f
Trishamaee <rabacatrisha@gmail.com>
2019-01-08 15:51:19 +0800

    Functioning Clear and Exit Button

--- 001-basic_elements/BasicElements.java
+++ 001-basic_elements/BasicElements.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
 public class BasicElements{
 	
@@ -6,6 +8,7 @@ public class BasicElements{
 		JFrame myFrame = new JFrame("Basic Java GUI");
 		JButton myButton = new JButton("Login");
 		JButton ExitButton = new JButton("Exit");
+		JButton Clear = new JButton("Clear");
 		JTextField UsernameTextField = new JTextField();
 		JTextField PasswordTextField = new JTextField();
 		JLabel UsernameLabel = new JLabel("Username:");
@@ -14,14 +17,14 @@ public class BasicElements{
 		myFrame.setVisible(true);
 		myFrame.setSize(500, 200);
 		myFrame.setLayout(null);
-
-	
+		
 		myFrame.add(myButton);
 		myFrame.add(ExitButton);
 		myFrame.add(UsernameTextField);
 		myFrame.add(PasswordTextField);
 		myFrame.add(UsernameLabel);
 		myFrame.add(PasswordLabel);
+		myFrame.add(Clear);
 
 		myButton.setVisible(true);
 		myButton.setSize(20,5);
@@ -29,6 +32,9 @@ public class BasicElements{
 
 		ExitButton.setSize(20,5);
 		ExitButton.setBounds(269, 100 , 80 , 20);
+
+		Clear.setSize(20,5);
+		Clear.setBounds(70, 100 , 80 , 20);
 		
 		UsernameLabel.setBounds(73,34, 20, 60);
 		UsernameLabel.setSize(200, 50);
@@ -41,6 +47,21 @@ public class BasicElements{
 
 		PasswordTextField.setBounds(150,70, 20, 30);
 		PasswordTextField.setSize(200, 20);
+
+		Clear.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e){
+				UsernameTextField.setText("");
+				PasswordTextField.setText("");
+			}
+
+		});
+
+		ExitButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e){
+				System.exit(0);
+			}
+
+		});
 
 		myFrame.setDefaultCloseOperation(myFrame.EXIT_ON_CLOSE);
 		System.out.println("basic java elements");
