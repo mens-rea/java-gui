e7b6c704e70934059f31cb978333c2d6733b8de1
seleneartemis <reesenicolerivera@gmail.com>
2019-01-08 15:09:16 +0800

    add login page

--- 001-basic_elements/BasicElements.java
+++ 001-basic_elements/BasicElements.java
@@ -4,16 +4,37 @@ public class BasicElements{
 	
 	public static void main(String args[]){
 		JFrame myFrame = new JFrame("Basic Java GUI");
+	
+		//Username Box
+		JLabel labelusername = new JLabel("Username:");
+		labelusername.setBounds(80,70,200,30);
+		myFrame.add(labelusername);
+
+		//textfield
+		JTextField textf = new JTextField();
+		textf.setBounds(150,70, 170, 30);
+		myFrame.add(textf);
+
+		//Password
+		JLabel labelpassword = new JLabel("Password:");
+		labelpassword.setBounds(80,110,200,30);
+		myFrame.add(labelpassword);
+
+		//textfield
+		JPasswordField textf1 = new JPasswordField();
+		textf1.setBounds(150, 110, 170, 30);
+		myFrame.add(textf1);
+
+		//Login Button
 		JButton myButton = new JButton("Login");
 		myButton.setSize(200,50);
-
+		myButton.setBounds(150, 160, 100, 30);
 		myFrame.setLayout(null);
-
 		myFrame.add(myButton);
 
 		myFrame.setDefaultCloseOperation(myFrame.EXIT_ON_CLOSE);
 		myFrame.setVisible(true);
-		myFrame.setSize(900, 600);
+		myFrame.setSize(420, 325);
 
 		System.out.println("basic java elements");
 	}
