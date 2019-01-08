53c9dab4bba8210351bf805eaed0a3e95f025a08
alicandoalyssa <fayealicando@gmail.com>
2019-01-08 15:54:32 +0800

    create login form

--- 001-basic_elements/BasicElements.java
+++ 001-basic_elements/BasicElements.java
@@ -1,21 +1,37 @@
 import javax.swing.*;
+import java.awt.FlowLayout;
 
 public class BasicElements{
-	
-	public static void main(String args[]){
-		JFrame myFrame = new JFrame("Basic Java GUI");
-		JButton myButton = new JButton("Login");
-		myButton.setSize(200,50);
 
-		myFrame.setLayout(null);
+    static JTextField text1, text2;
+    
+    public static void main(String args[]){
 
-		myFrame.add(myButton);
+        JFrame myFrame = new JFrame("Basic Java GUI");
+        myFrame.getContentPane().setLayout(new FlowLayout());
 
-		myFrame.setDefaultCloseOperation(myFrame.EXIT_ON_CLOSE);
-		myFrame.setVisible(true);
-		myFrame.setSize(900, 600);
+        text1 = new JTextField("Username",10);
+        text2 = new JPasswordField("",10);
+        myFrame.getContentPane().add(text1);
+        myFrame.getContentPane().add(text2);
 
-		System.out.println("basic java elements");
-	}
+        JButton myButton1 = new JButton("Login");
+        myButton1.setSize(200,50);
+        myButton1.setBounds(300, 300, 120, 28);
+        JButton myButton2 = new JButton("Exit");
+        myButton2.setBounds(450, 300, 120, 28);
+
+        myFrame.add(myButton1);
+        myFrame.add(myButton2);
+
+        myFrame.pack();
+        myFrame.setLayout(null);
+        myFrame.setDefaultCloseOperation(myFrame.EXIT_ON_CLOSE);
+        myFrame.setVisible(true);
+        myFrame.setSize(400, 100);
+
+        System.out.println("basic java elements");
+    }
 
 }
+        
