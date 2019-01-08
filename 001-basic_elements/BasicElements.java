35a5acb31f9c0378b5683cd42b23bb10b6e874cf
chubzielex <reyes.lexus@gmail.com>
2019-01-08 14:23:13 +0800

    add labels and text fields

--- 001-basic_elements/BasicElements.java
+++ 001-basic_elements/BasicElements.java
@@ -1,15 +1,49 @@
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
 
 public class BasicElements{
 	
 	public static void main(String args[]){
-		JFrame myFrame = new JFrame();
+		JFrame frame = new JFrame("Login");
 
-		myFrame.setDefaultCloseOperation(myFrame.EXIT_ON_CLOSE);
-		myFrame.setVisible(true);
-		myFrame.setSize(900, 600);
+        frame.setSize(300, 150);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		System.out.println("basic java elements");
-	}
+        JPanel panel = new JPanel();
+        frame.add(panel);
+        placeComponents(panel);
+      
+
+        frame.setVisible(true);
+    }
+
+    private static void placeComponents(JPanel panel) {
+
+        panel.setLayout(null);
+        panel.setBackground(Color.black);
+
+        JLabel userLabel = new JLabel("Username:");
+        userLabel.setBounds(10, 10, 80, 25);
+        userLabel.setForeground(Color.white);
+        panel.add(userLabel);
+
+        JTextField userText = new JTextField(20);
+        userText.setBounds(100, 10, 160, 25);
+        panel.add(userText);
+
+        JLabel passwordLabel = new JLabel("Password:");
+        passwordLabel.setBounds(10, 40, 80, 25);
+        passwordLabel.setForeground(Color.white);
+        panel.add(passwordLabel);
+
+        JPasswordField passwordText = new JPasswordField(20);
+        passwordText.setBounds(100, 40, 160, 25);
+        panel.add(passwordText);
+
+        JButton loginButton = new JButton("login");
+        loginButton.setBounds(10, 80, 80, 25);
+        panel.add(loginButton);
+        
+    }
 
 }
